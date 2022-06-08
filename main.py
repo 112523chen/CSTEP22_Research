5c006c0ece7867de559e59442205e150ba2376f4
112523chen <112623chen@gmail.com>
2022-06-08 17:35:57 -0400

    .

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = pd.read_csv('data.csv')
+
+current_columns = (list(data.columns))
+new_column_order = ['file', 'company_name', 'title', 'question_type', 'python_difficulty',
+                    'mySQL_difficulty', 'postgreSQL_difficulty', 'prompt', 'source', 'question_id']
+
+df = data[new_column_order]
+
+# df.to_csv('data_.csv')
