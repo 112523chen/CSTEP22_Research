2b407260902759104b323adc5ff30d6acb8ca73c
112523chen <112623chen@gmail.com>
2022-06-16 12:48:04 -0400

    .

--- Functions.py
+++ Functions.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 
 def getDatasetAsDataFrame(fileName):
-    os.chdir("../datasets")
+    os.chdir("./datasets")
+    print(os.getcwd())
     df = pd.read_csv(fileName)
     os.chdir("../")
+    return df
