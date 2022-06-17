c519147b16266d1b4ed7d9f63af308eedc51a59b
112523chen <112623chen@gmail.com>
2022-06-17 11:35:48 -0400

    .

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import ast
 from st_aggrid import GridUpdateMode, DataReturnMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid import AgGrid
 import streamlit as st
 import pandas as pd
+
+from Functions import getDatasetAsDataFrame
 from Questions import Question
 logic_df = pd.read_csv('data_.csv')
+
 
 # stores nested dictionary
 datasets_by_company = dict()
@@ -23,7 +27,7 @@ for company in companies:
     if company not in datasets_by_company.keys():
         datasets_by_company[company] = dict()
 
-
+# inputs values of question class into the nested dictionary based on company
 for question_id in question_IDs:
     question_id_df = logic_df[logic_df['question_id'] == question_id]
     title = question_id_df['title'].unique()[0]
@@ -32,7 +36,7 @@ for question_id in question_IDs:
     question_type = question_id_df['question_type'].unique()[0]
     python_difficulty = question_id_df['python_difficulty'].unique()[0]
     sql_difficulty = question_id_df['mySQL_difficulty'].unique()[0]
-    filesNames = question_id_df['file'].unique()
+    filesNames = list(question_id_df['file'].unique())
 
     f = title.split()
     f = map(str.capitalize, f)
@@ -43,13 +47,12 @@ for question_id in question_IDs:
 
     datasets_by_company[company_name][f] = question
 
+# creates a dataframe of interview question from dictionary
 data = dict()
 for c in datasets_by_company.keys():
     for d in datasets_by_company[c].keys():
         sub_data = datasets_by_company[c][d]
         data[d] = vars(sub_data)
-
-
 datas = pd.DataFrame(data).T
 
 ################################################################################################################################################################
@@ -61,14 +64,17 @@ st.write("""
 # Interview Questions
 """)
 
+# selection box for company 
 companies_ = list(logic_df["company_name"].unique())
 companies_.insert(0, "All")
 company_answer = st.selectbox("Select Company Name", companies_)
 
+# selection box for difficulty of question based on company input
 difficulties_ = list(logic_df[logic_df.company_name == company_answer]["python_difficulty"].unique())
 difficulties_.insert(0, "All")
 difficulty_answer = st.selectbox("Select Difficulty", map(str.capitalize,difficulties_))
 
+#creates logic for main table
 df = datas
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_selection(selection_mode="single", use_checkbox=True)
@@ -89,6 +95,7 @@ elif(difficulty_answer == "Easy"):
 
 df.reset_index(inplace=True, drop=True)
 
+# creates actual table
 response = AgGrid(
     df,
     gridOptions=gridOptions,
@@ -97,9 +104,15 @@ response = AgGrid(
     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
 )
 
-df_ = pd.DataFrame(response["selected_rows"])
-st.write(df_)
+# info = response["selected_rows"]
+# if(len(info) == 1):
+#     df_ = pd.DataFrame(info)
+#     st.write(df_)
 
 usedDataSet = response["selected_rows"][0]["fileNames"]
-
-
+usedDataSet = ast.literal_eval(usedDataSet)
+st.write(usedDataSet)
+# for f in usedDataSet:
+#     continue
+#     # df = getDatasetAsDataFrame(f)
+#     # st.write(df.head())
