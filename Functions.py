ff36c0595872940e1e773c99a05bd86240863fc2
112523chen <112623chen@gmail.com>
2022-06-20 12:13:00 -0400

    added More Functions

--- Functions.py
+++ Functions.py
@@ -3,7 +3,32 @@ import pandas as pd
 
 def getDatasetAsDataFrame(fileName):
     os.chdir("./datasets")
-    print(os.getcwd())
+    # print(os.getcwd())
     df = pd.read_csv(fileName)
     os.chdir("../")
+    # print(os.getcwd())
     return df
+
+def getPythonSolution(question):
+    os.chdir("./Solutions/Python")
+    f = f"{question}.py"
+    text_file = open(f,'r')
+    python_solution = text_file.read()
+    text_file.close()
+    os.chdir("../..")
+    return python_solution
+
+def getSQLSolution(question):
+    os.chdir("./Solutions/SQL")
+    f = f"{question}.sql"
+    text_file = open(f,"r")
+    sql_solution = text_file.read()
+    text_file.close()
+    os.chdir("../..")
+    return(sql_solution)
+
+def getSolutions(question):
+    python_solution = getPythonSolution(question)
+    sql_solution = getSQLSolution(question)
+    solution = f"Python Solution:\n{python_solution} \n\nSQL Solution:\n{sql_solution}"
+    return solution
